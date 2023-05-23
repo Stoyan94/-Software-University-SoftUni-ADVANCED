7f618e9e814077c318e758539563d46abe447ed9
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-05-23 22:20:02 +0300

    ex

---
+++ StackAndQueue/07.Queues-HotPotato/07.Queues-HotPotato/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.Queues_HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] peopels = Console.ReadLine().Split();
+            int tossesCount = int.Parse(Console.ReadLine());
+
+            Queue<string> kids = new Queue<string>(peopels);
+
+            int crrTosses = 1;
+            
+            while (kids.Count>1)
+            {
+                string kidWithPopato = kids.Dequeue();               
+                if (crrTosses!=tossesCount)
+                {
+                    crrTosses++;
+                    kids.Enqueue(kidWithPopato);
+                }
+                else
+                {
+                    crrTosses = 1;
+                    Console.WriteLine($"Removed {kidWithPopato}");
+                }
+
+            }
+
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+        }
+    }
+}
