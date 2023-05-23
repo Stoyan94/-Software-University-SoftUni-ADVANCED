1024c46e4cb758a5f47531494e54d8a2dbf15f7f
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-05-23 22:19:41 +0300

    ex

---
+++ StackAndQueue/06.Queues-Supermarket/06.Queues-Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Queues_Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> paidCustomers = new Queue<string>();
+            string customores;
+
+            while ((customores = Console.ReadLine())!="End")
+            {
+                if (customores=="Paid")
+                {
+                    foreach (var customer in paidCustomers)
+                    {
+                        Console.WriteLine(customer);
+                    }
+                    paidCustomers.Clear();
+                }
+                else
+                {
+                    paidCustomers.Enqueue(customores);
+                }                
+            }
+
+            Console.WriteLine($"{paidCustomers.Count} people remaining.");
+        }
+    }
+}
