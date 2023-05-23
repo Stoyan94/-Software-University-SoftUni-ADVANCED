40975701c81dafd8c1ac1885863326ac3e5d3298
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-05-23 22:17:17 +0300

    new

---
+++ StackAndQueue/03.SimpleCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textCalculator = Console.ReadLine();
+            string[] reversed = textCalculator.Split(' ').Reverse().ToArray();
+            Stack<string> calculator = new Stack<string>(reversed);
+            int sum = 0;
+
+            while (calculator.Count>0)
+            {
+                if (calculator.Peek()=="-")
+                {
+                    calculator.Pop();
+                    sum -= int.Parse(calculator.Peek());
+                    calculator.Pop();
+                }
+                else if (calculator.Peek()=="+")
+                {
+                    calculator.Pop();
+                    sum += int.Parse(calculator.Peek());
+                    calculator.Pop();
+                }
+                else
+                {
+                    sum+= int.Parse(calculator.Peek());
+                    calculator.Pop();
+                }
+                
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
