56479f7cee0bdc4d7db2a469183633b91feeab3a
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-05-25 14:09:10 +0300

    ex

---
+++ StackAndQueue-ADVANCE/02.Queue-Advance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02.Queue_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int preparedDishes = int.Parse(Console.ReadLine());
+            int[] platesOrder = Console.ReadLine().Split(' ')
+                .Select(int.Parse).ToArray();
+
+            Queue<int> orders = new Queue<int>(platesOrder);
+
+            int biggestOrder = orders.Max();
+            Console.WriteLine(biggestOrder);
+
+            for (int i = 0; i < platesOrder.Length; i++)
+            {
+                int currOrder = platesOrder[i];
+
+                if (preparedDishes-currOrder <0)
+                {
+                    break;              
+                }
+                preparedDishes -= currOrder;
+                orders.Dequeue();
+            }
+
+            if (orders.Count>0)
+            {                
+                Console.WriteLine($"Orders left: {string.Join(" ",orders)}");
+            }
+            else
+            {               
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
