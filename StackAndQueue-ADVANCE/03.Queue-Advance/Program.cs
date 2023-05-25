d2b8cc3de8019aa6c119749b1d636d02b06529b1
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-05-25 14:10:21 +0300

    ex

---
+++ StackAndQueue-ADVANCE/03.Queue-Advance/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Queue_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputSongs= Console.ReadLine().Split(", ");
+
+            Queue<string> listSongs = new Queue<string>(inputSongs);
+
+            while (listSongs.Count>0)
+            {
+                string [] commands = Console.ReadLine().Split(' ',
+                    StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string command = commands[0];
+
+                if (command=="Add")
+                {
+                    
+                    AddSong(listSongs,commands);
+                }
+                else if (command == "Play")
+                {
+                    listSongs.Dequeue();
+                }
+                else if (command=="Show")
+                {
+                    ShowSongs(listSongs);
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+
+        private static void ShowSongs(Queue<string> listSongs)
+        {
+            string songs = string.Join(", ", listSongs);
+            Console.WriteLine(songs);
+        }
+
+        private static void AddSong(Queue<string> listSongs,string [] songName)
+        {
+            string currSong = string.Join(" ",songName.Skip(1));
+
+            if (!listSongs.Contains(currSong))
+            {
+                listSongs.Enqueue(currSong);
+            }
+            else
+            {
+                Console.WriteLine($"{currSong} is already contained!");
+            }
+        }
+    }
+}
