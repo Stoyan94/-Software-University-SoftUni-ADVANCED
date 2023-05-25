222460b4b2367e623cb62a1f4050abc926e31c1e
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-05-25 14:11:27 +0300

    ex

---
+++ StackAndQueue-ADVANCE/03.Stack-Advance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03.Stack_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputInfo = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+
+            Stack<int> box = new Stack<int>(inputInfo);
+
+            int racksUsed = 0;
+            int currentRackCapacity = 0;
+            while (box.Count > 0)
+            {
+                if (currentRackCapacity - box.Peek() < 0)
+                {
+                    racksUsed++;
+                    currentRackCapacity = capacity;
+                }
+                else
+                {
+                    currentRackCapacity -= box.Pop();
+                }
+            }
+            Console.WriteLine(racksUsed);
+        }
+    }
+}
