ff1e23b17235b1d10bb8480c57a46b8d36c3c639
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-05-25 14:08:32 +0300

    ex

---
+++ StackAndQueue-ADVANCE/01.Stack-Advance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Stack_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] stackOperations =Console.ReadLine().Split(' ').Select(int.Parse)
+                .ToArray();
+            int [] numsToAdd =Console.ReadLine().Split(' ').Select(int.Parse)
+                .ToArray();
+
+            Stack<int> numbers = new Stack<int>();
+
+            int timeAdds = stackOperations[0];
+
+            for (int i = 0; i < timeAdds; i++)
+            {
+                 numbers.Push(numsToAdd[i]);
+
+            }
+            int operationsLeft = 0;
+
+            while (stackOperations[1] !=operationsLeft)
+            {
+                operationsLeft++;
+
+                numbers.Pop();
+            }
+
+            if (numbers.Contains(stackOperations[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else if(numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
