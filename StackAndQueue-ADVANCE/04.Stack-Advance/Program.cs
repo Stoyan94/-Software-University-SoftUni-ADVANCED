4bc0df263bf8a6345f830ad52684abd85f769bb4
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-01 16:56:34 +0300

    ex

---
+++ StackAndQueue-ADVANCE/04.Stack-Advance/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace _04.Stack_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool isBalanced = true;
+            Stack<char> parantheses = new Stack<char>();
+
+            foreach (var item in input)
+            {
+
+                if (item == '(' ||
+                    item == '[' ||
+                    item =='{' )
+                {
+                    parantheses.Push(item);
+                    continue;
+                }
+
+                if (parantheses.Count <= 0)
+                {
+                    isBalanced = false;
+                    break;
+                }
+
+                char currChar = parantheses.Peek();
+
+                if (item==')' && parantheses.Peek() =='(')
+                {
+                    parantheses.Pop();
+                }
+                else if (item == '}' && parantheses.Peek() == '{')
+                {
+                    parantheses.Pop();
+                }
+                else if (item == ']' && parantheses.Peek() =='[')
+                {
+                    parantheses.Pop();
+                }
+                else
+                {
+                    isBalanced = false;
+                    break;
+                }               
+            }
+
+            if (!isBalanced ||parantheses.Count > 0)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
