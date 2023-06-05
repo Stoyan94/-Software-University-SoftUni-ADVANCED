ca5ff3e3c6c8f35534f75472c72c7394459b6d6e
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-05 19:12:02 +0300

    ex

---
+++ Multidimensional Arrays/08.Diagonal-Diffrence/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.Diagonal_Diffrence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int [,] matrix = ReadMatrix(size);
+
+            int rightLeft = 0;
+            int leftRight = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                rightLeft += matrix[i, i];
+                leftRight += matrix[i,matrix.GetLength(1)-i-1];
+            }
+
+            Console.WriteLine($"{Math.Abs(rightLeft-leftRight)}");
+        }
+
+        private static int[,] ReadMatrix(int size)
+        {
+            int [,] matrix = new int[size,size];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
