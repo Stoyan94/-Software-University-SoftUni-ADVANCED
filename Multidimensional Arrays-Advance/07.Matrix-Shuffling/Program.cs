3cc812cbaffa0692a7e93b068b3513d6add610a3
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-09 19:33:57 +0300

    ex

---
+++ Multidimensional Arrays-Advance/07.Matrix-Shuffling/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _11.Matrix_Shuffling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] matrixSize = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            string[,] matrix = ReadMatrix(matrixSize);
+
+            string inputComand;
+
+            while ((inputComand=Console.ReadLine())!="END")
+            {
+                var cmdArgs = inputComand.Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                var command = cmdArgs[0];              
+
+                if (command=="swap")
+                {
+                    var firstRow = int.Parse(cmdArgs[1]);
+                    var firstCol = int.Parse(cmdArgs[2]);
+                    var seckondRow = int.Parse(cmdArgs[3]);
+                    var seckondCol = int.Parse(cmdArgs[4]);
+
+                    if (!isInRange(matrix, firstRow, firstCol, seckondRow, seckondCol))
+                    {
+                        
+
+                        Console.WriteLine($"Invalid input!");
+                        continue;
+                    }
+                    string firstSwap = matrix[firstRow, firstCol];
+                    string seckondSwap= matrix[seckondRow, seckondCol];
+                    matrix[seckondRow, seckondCol] = firstSwap;
+                    matrix[firstRow, firstCol] = seckondSwap;
+
+                    PrintChage(matrix);
+                }
+                else 
+                {
+                    Console.WriteLine($"Invalid input!");
+                }
+            }
+        }
+
+        private static bool isInRange(string[,] matrix, int firstRow, int firstCol, int seckondRow, int seckondCol)
+        {
+            return firstRow >= 0 && firstRow < matrix.GetLength(0) &&
+            firstCol >= 0 && firstCol < matrix.GetLength(1) &&
+            seckondRow >= 0 && seckondRow < matrix.GetLength(0)&&
+            seckondCol >= 0 && seckondCol < matrix.GetLength(1);
+
+
+        }
+
+        private static void PrintChage(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static string[,] ReadMatrix(int[] matrixSize)
+        {
+            string[,] matrix = new string[matrixSize[0], matrixSize[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string [] input= Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row,col] = input[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Multidimensional Arrays-Advance/Bonus-Snake-Moves/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _12.Snake_Moves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] matrixSize = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            
+            string snakeSymnols = Console.ReadLine();
+
+            char[,] field = new char [matrixSize[0],matrixSize[1]];
+
+            bool rightLeft = true;
+            int indexOfSymbol = 0;
+
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                if (rightLeft)
+                {
+                    for (int col = 0; col < field.GetLength(1); col++)
+                    {
+                        field[row, col] = snakeSymnols[indexOfSymbol++];
+
+                        if (indexOfSymbol==snakeSymnols.Length)
+                        {
+                            indexOfSymbol = 0;
+                        }
+                    }
+
+                    rightLeft = false;
+                }
+                else
+                {
+                    for (int col = field.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        field[row, col] = snakeSymnols[indexOfSymbol++];
+
+                        if (indexOfSymbol == snakeSymnols.Length)
+                        {
+                            indexOfSymbol = 0;
+                        }
+                    }
+                    rightLeft= true;
+                }
+            }
+
+            for (int rol = 0; rol < field.GetLength(0); rol++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    Console.Write(field[rol, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
