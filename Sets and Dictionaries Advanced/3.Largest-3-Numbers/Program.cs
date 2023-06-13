e1f9494ceea7da11de72b467d59cf9728043f0fb
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-13 14:36:12 +0300

    ex

---
+++ Sets and Dictionaries Advanced/3.Largest-3-Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int[] sortedNum = numbers.OrderByDescending(x => x).Take(3).ToArray();
+
+            Console.WriteLine(String.Join(" ",sortedNum));
+        }
+    }
+}
