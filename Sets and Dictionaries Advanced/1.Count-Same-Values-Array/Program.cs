c0c5bfbf115d62d87ba8ebab0b6df6ff42a89b93
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-13 14:34:37 +0300

    ex

---
+++ Sets and Dictionaries Advanced/1.Count-Same-Values-Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _1.Count_Same_Values_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+
+            Dictionary<double,int> repeat = new Dictionary<double,int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (!repeat.ContainsKey(array[i]))
+                {
+                    repeat.Add(array[i], 0);
+                }
+
+                repeat[array[i]]++;
+            }
+
+            foreach (var item in repeat)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
