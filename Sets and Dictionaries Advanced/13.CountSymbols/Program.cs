ad2f96250308d4d48a00b484d8d23624537f997b
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-18 09:38:02 +0300

    ex

---
+++ Sets and Dictionaries Advanced/13.CountSymbols/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _13.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+            FillSymbols(symbols);
+
+            foreach (var currSymbol in symbols)
+            {
+                Console.WriteLine($"{currSymbol.Key}: {currSymbol.Value} time/s");
+            }
+
+             //: 1 time / s
+             //S: 1 time / s
+
+        }
+
+        private static SortedDictionary<char, int> FillSymbols(SortedDictionary<char, int> symbols)
+        {
+            string text = Console.ReadLine();            
+
+            for (int i = 0; i <text.Length; i++)
+            {
+                if (!symbols.ContainsKey(text[i]))
+                {
+                    symbols.Add(text[i], 0);
+                }
+                symbols[text[i]]++;
+            }
+           
+            return symbols;
+        }
+    }
+}
