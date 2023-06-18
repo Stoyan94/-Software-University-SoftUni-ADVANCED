3a7f6e0dd871a156ec923013beae9301bda551b6
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-18 09:36:10 +0300

    ex

---
+++ Sets and Dictionaries Advanced/9 Unique-Usernames/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _9_Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int nNmaes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nNmaes; i++)
+            {
+                string name = Console.ReadLine();
+
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
