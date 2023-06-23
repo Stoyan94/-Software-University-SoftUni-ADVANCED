cc2be5f6284ac2ec8a8ca484b4c61d8e2c0fae57
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-23 16:30:25 +0300

    ex

---
+++ Functional Programming/03.Count-Uppercase-Words/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] text = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> isStartWithCapital = w => char.IsUpper(w[0]);
+            
+            text= text.Where(isStartWithCapital).ToArray();
+
+            Action<string[]> textPrint = x => 
+            Console.WriteLine((string.Join(Environment.NewLine, x)));
+
+            textPrint(text);        
+
+            
+        }
+    }
+}
