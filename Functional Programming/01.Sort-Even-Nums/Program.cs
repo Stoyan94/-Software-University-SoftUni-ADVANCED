dad506cacd87b59aec3fe30d5311d7a6795fd38c
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-23 16:29:37 +0300

    ex

---
+++ Functional Programming/01.Sort-Even-Nums/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01.Sort_Even_Nums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split (",").
+                Select(int.Parse).ToList();
+
+            Predicate<int> even = x=> x % 2 == 0;            
+
+            nums = nums.FindAll(even).OrderBy(x=>x).ToList();
+
+            Action<List<int>> print = x => Console.WriteLine(String.Join(", ",x));
+
+            print(nums);
+
+        }
+    }
+}
