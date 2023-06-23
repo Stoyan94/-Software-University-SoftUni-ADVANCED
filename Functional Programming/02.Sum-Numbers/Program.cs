31bef986ba691cfee6352e3303300943c8091b57
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-23 16:30:06 +0300

    ex

---
+++ Functional Programming/02.Sum-Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(", ").
+                Select(int.Parse).ToArray();
+
+            Func<int[], int> sumNums = x=> x.Sum();
+            Func<int[],int> lenght = x=> x.Length;
+
+            Console.WriteLine(lenght(nums));
+            Console.WriteLine(sumNums(nums));            
+           
+        }
+    }
+}
