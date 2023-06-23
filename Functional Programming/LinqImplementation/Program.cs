cc96988e5776eeff71792e648ee9172066b232d2
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-23 16:33:05 +0300

    ex

---
+++ Functional Programming/LinqImplementation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinqImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
+
+            //numbers = numbers.Where(x =>x >3).ToList();
+
+            numbers = Where(numbers, x => x > 3);
+
+            Console.WriteLine(String.Join(",",numbers));
+        }
+
+        public static List<int> Where(List<int> list ,Func<int, bool> cheker)
+        {
+            List<int> result = new List<int>();
+
+            foreach (var item in list)
+            {
+                if (cheker(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+    }
+}
