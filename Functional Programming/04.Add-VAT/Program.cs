aed8e1e86ae208050c9ed8beaa9dd23252551e74
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-06-23 16:30:45 +0300

    ex

---
+++ Functional Programming/04.Add-VAT/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04.Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double [] numms = Console.ReadLine().Split(",")
+                .Select(double.Parse).ToArray();
+                
+            Action<double[]> action = x =>
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    x[i] += (x[i] * 0.20);
+                }
+            };
+
+            Action<double[]> print = x =>
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    Console.WriteLine($"{x[i]:F2}");
+                }
+            };
+
+            action(numms);
+            print(numms);
+
+           
+        }
+    }
+}
