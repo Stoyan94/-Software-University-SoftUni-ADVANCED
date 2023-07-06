e024a946b1900f2a83049dbcabb42f63c6703a76
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-06 09:18:49 +0300

    ex

---
+++ Functional Programming-EX/04.Find-Even-or-Odds/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Find_Even_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            int[] ranges = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = ranges[0]; i <=ranges[1]; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Predicate<int> even = evenNums=> evenNums % 2 == 0;
+
+            Predicate<int> odd = oddNums => oddNums % 2 != 0;
+
+            Action<List<int>> print = nums=>
+            Console.WriteLine(String.Join(" ",nums));
+
+            string command = Console.ReadLine();
+
+            if (command=="even")
+            {
+                numbers= numbers.FindAll(even);
+            }
+            else if (command == "odd")
+            {
+                numbers = numbers.FindAll(odd);
+            }
+
+            print(numbers);
+        }
+    }
+}
