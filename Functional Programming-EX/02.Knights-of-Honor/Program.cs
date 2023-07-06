a2a5c7ef81e964962b072d013b33f88f1efa000d
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-06 09:16:45 +0300

    ex

---
+++ Functional Programming-EX/02.Knights-of-Honor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02.Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printNames = allNames =>
+            Console.WriteLine("Sir "+ string.Join(Environment.NewLine+ "Sir ",allNames));
+
+            string[] names = Console.ReadLine().Split();
+
+            printNames(names);
+        }
+    }
+}
