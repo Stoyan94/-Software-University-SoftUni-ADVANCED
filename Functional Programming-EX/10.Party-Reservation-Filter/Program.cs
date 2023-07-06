10880f6c897951c26c55ac2c78bc377a8ab8324c
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-06 09:22:34 +0300

    ex

---
+++ Functional Programming-EX/10.Party-Reservation-Filter/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _10.Party_Reservation_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(' ').ToList();
+
+            string action = Console.ReadLine();
+
+            Dictionary<string,Predicate<string>> allFilters = 
+                new Dictionary<string,Predicate<string>>();
+
+            while (action != "Print")
+            {
+                string[] items = action.Split(";");
+
+                string method = items[0];
+                string operation = items[1];
+                string value = items[2];
+
+
+                if (method =="Add filter")
+                {
+                    allFilters.Add(operation + value,GetPredicate(operation,value));
+                }
+                else
+                {
+                    allFilters.Remove(operation + value);
+                }
+
+                action = Console.ReadLine();
+            }
+
+            foreach (var filter in allFilters)
+            {
+                names.RemoveAll(filter.Value);
+            }
+
+            Console.Write(String.Join(" ",names));
+        }
+
+        private static Predicate<string> GetPredicate(string operation, string value)
+        {
+            if (operation == "Starts with")
+            {
+                return x => x.StartsWith(value);
+            }
+            else if (operation == "Ends with")
+            {
+                return x => x.EndsWith(value);
+            }
+            else if (operation=="Contains")
+            {
+                return x => x.Contains(value);
+            }
+
+            
+
+            int valueAsInt = int.Parse(value);
+
+            return x => x.Length == valueAsInt;
+        }
+    }
+}
