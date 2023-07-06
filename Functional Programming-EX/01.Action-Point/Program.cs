1df7d58378288a6744b289b5a7050ab4afdf37f3
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-06 09:16:07 +0300

    ex

---
+++ Functional Programming-EX/01.Action-Point/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] names = Console.ReadLine().Split();
+            
+            Action<string[]> print = names=> 
+            Console.WriteLine(string.Join(Environment.NewLine,names));
+
+            print(names);
+        }
+    }
+}
