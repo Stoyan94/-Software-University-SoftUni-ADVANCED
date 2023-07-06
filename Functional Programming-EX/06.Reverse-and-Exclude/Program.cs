9a6b5b30f3a508fd723835aa1cca0afda781190d
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-06 09:20:22 +0300

    ex

---
+++ Functional Programming-EX/06.Reverse-and-Exclude/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _06.Reverse_and_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numbers = Console.ReadLine().Split(' ')
+                .Select(int.Parse)
+                .Reverse().ToArray();
+
+            int divNum = int.Parse(Console.ReadLine());
+
+            Func<int, int, bool> divByN = (x, y) => x % y != 0;
+
+            Action<List<int>> print = result =>
+            Console.WriteLine(String.Join(" ",result));
+
+            List<int> result =  numbers.Where(x=> divByN(x,divNum)).ToList();
+
+            print(result);
+        }
+    }
+}
