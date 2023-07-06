418f93641e8ba4682470177bf98f1ba408f0dfdd
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-06 09:20:51 +0300

    ex

---
+++ Functional Programming-EX/07.Predicate-For-Names/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+
+            int nameLeght = int.Parse(Console.ReadLine());
+
+            string [] names = Console.ReadLine().Split(' ');
+
+            Func<string, int, bool> leghtName = (x, y) => x.Length <= y;            
+
+            string [] resultNames = names.Where(x=>(x.Length <=nameLeght)).ToArray();           
+            
+            Action<string[]> action = print => Console.WriteLine(string.Join(Environment.NewLine, print));
+
+            action(resultNames);
+        }
+    }
+}
