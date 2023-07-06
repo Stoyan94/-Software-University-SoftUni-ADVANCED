a294525fe6e171ad46c74a26d312bd2bf1d8c852
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-06 09:17:10 +0300

    ex

---
+++ Functional Programming-EX/03.Custom-Min-Function/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03.Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> getMinNum = numbers =>
+             numbers.Min();
+
+            int [] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+             int minNum =  getMinNum(numbers);
+
+
+            Console.WriteLine(minNum);
+        }
+    }
+}
