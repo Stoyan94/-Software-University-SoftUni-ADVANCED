032d64ae2d15b782f4841f4bfb06a1781ac96545
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-11 14:50:34 +0300

    ex

---
+++ Defining Classes/Defining-Classes/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defining_Classes
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+
+        public string Company { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Milage { get; set; }
+        
+
+        public void Drive(int km)
+        {
+            Milage += km;
+            Console.WriteLine($"Car {Name} {Company} driven for {Milage} km");
+        }
+    }
+}
---
+++ Defining Classes/Defining-Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Defining_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car bwm = new Car()
+            {
+                Name ="3",
+                Company = "BWM",
+                Category = "Cars",
+                Price = 3000
+            };
+            bwm.Drive(100);
+            bwm.Drive(20);
+
+            Car audi = new Car()
+            {
+                Name = "a3",
+                Company = "Audi",
+                Category = "Cars",
+                Price = 3500
+            };
+            audi.Drive(150);
+            audi.Drive(25);
+
+            Console.WriteLine($"Car: {bwm.Name}, {bwm.Price}, {bwm.Company} - Milage {bwm.Milage}km");
+
+            Console.WriteLine($"Car: {audi.Name}, {audi.Price}, {audi.Company} - Milage {audi.Milage}km");
+        }
+    }
+}
