5e9bfae0fd72a17562d3722dbe2f940b8b3a1dae
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-11 14:49:57 +0300

    ex

---
+++ Defining Classes/Constructors/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Animal
+    {
+        public Animal(string species,bool isHungry)
+        {
+            Console.WriteLine($"I am in constructor");          
+            Species = species;           
+            IsHungry = isHungry;
+        }
+
+        //invoke ctro in ctro
+        public Animal(string name, string gender,string species, bool isHungry) : this(species,isHungry)
+        {
+            Name = name;
+            Gender = gender;
+        }           
+          
+
+        public string Species { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public bool IsHungry { get; set; }
+    }
+}
---
+++ Defining Classes/Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //one ctro
+            Animal animal = new Animal("Hippo",true);
+
+            //ctron in ctro
+            Animal ctro2 = new Animal("Hippo", "Female", "hippo", true);
+
+            Console.WriteLine($"I am after constructor");
+
+           
+
+            Console.WriteLine(ctro2.Name);
+            Console.WriteLine(ctro2.Species);
+            Console.WriteLine(ctro2.Gender);
+            Console.WriteLine(ctro2.IsHungry);
+        }
+    }
+}
