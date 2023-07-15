de0432a35c60edb19b4fa13f53b086c7c986562b
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-15 18:17:49 +0300

    ex

---
+++ Defining Classes-EX/DefiningClasses/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            Femaly = new List<Person>();
+        }
+        public List<Person> Femaly { get; set; }
+
+        public void AddMemer(Person person)
+        {
+            Femaly.Add(person);
+        }
+
+        public List<Person> GetOldestMember()
+        {
+            List<Person> person = Femaly.FindAll(p => p.Age>=30).OrderBy(n=>n.Name).ToList();           
+
+            return person;
+        }
+    }
+}
---
+++ Defining Classes-EX/DefiningClasses/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+     public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person person = new Person(name,age);
+                
+                family.AddMemer(person);
+            }
+
+            List<Person> oldest = family.GetOldestMember();
+
+            foreach (var people in oldest)
+            {
+                Console.WriteLine($"{people.Name} - {people.Age}");
+            }
+        }
+    }
+}
