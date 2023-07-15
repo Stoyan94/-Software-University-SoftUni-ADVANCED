4033dd7de8277313c2d86f5a950097d3d066fbc7
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-15 18:14:17 +0300

    ex

---
+++ Defining Classes-EX/06.Speed-Racing/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmout;
+        private double fuelConsumpionPerKilometer;
+        private double travelDistance;        
+      
+        public string Model { get { return model; } set { model = value; } }
+
+        public double FuelAmount { get { return fuelAmout; } set {fuelAmout=value; } }
+
+        public double FuelConsumptionPerKilometer { get { return fuelConsumpionPerKilometer; }
+            set { fuelConsumpionPerKilometer = value; } }
+
+        public double TravelDistance { get { return travelDistance; } set { travelDistance = value; } }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer
+            )
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.TravelDistance = 0;
+        }
+        public  void Drive(double travelDistance)
+        {
+            //double travelDistance = 0;
+            
+            if (this.FuelAmount- ((this.FuelConsumptionPerKilometer * travelDistance))>0)
+            {
+                this.FuelAmount -= (this.FuelConsumptionPerKilometer * travelDistance);
+                this.TravelDistance += travelDistance;
+            }
+            else
+            {
+                Console.WriteLine($"Insufficient fuel for the drive");
+            }
+            
+           
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:F2} {TravelDistance}";
+        }
+    }
+
+
+
+}
---
+++ Defining Classes-EX/06.Speed-Racing/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+           List<Car> cars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string carModel = input[0];
+                double fuelAmount = double.Parse(input[1]);
+                double fuelComsumpion = double.Parse(input[2]);
+
+                Car car = new Car(carModel,fuelAmount,fuelComsumpion);
+                
+                
+                cars.Add(car);
+            }
+
+            string carDrive;
+
+            while ((carDrive=Console.ReadLine())!="End")
+            {               
+
+                string carModel = carDrive.Split().Skip(1).First();
+                int travelDistance= int.Parse(carDrive.Split().Last());
+
+                Car currCar = cars.FirstOrDefault(x=>x.Model ==carModel);
+                currCar.Drive(travelDistance);
+                
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
