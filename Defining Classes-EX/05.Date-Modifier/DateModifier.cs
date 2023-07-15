a71c820f2048b6240383f247c706814c1561c17b
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-15 18:13:27 +0300

    ex

---
+++ Defining Classes-EX/05.Date-Modifier/DateModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Date_Modifier
+{
+    public class DateModifier
+    {
+        public static int CalculateDate(string firstDate,string seckondDate)
+        {
+            DateTime startDate = DateTime.Parse(firstDate);
+            DateTime endDate = DateTime.Parse(seckondDate);
+
+            int result = (int)Math.Abs((startDate-endDate).TotalDays);
+
+            return result;
+        }
+    }
+}
---
+++ Defining Classes-EX/05.Date-Modifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _05.Date_Modifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            int result =  DateModifier.CalculateDate(startDate, endDate);
+
+            Console.WriteLine(result);
+        }
+    }
+}
