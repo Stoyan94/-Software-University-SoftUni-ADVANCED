73a39dcce0393af56ebd46922ef6df1148e5056a
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-07-19 10:49:30 +0300

    ex

---
+++ Iterators and Comparators-EX/04.Froggy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04.Froggy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> elements = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            Lake lake = new Lake(elements);
+
+            Console.WriteLine(string.Join(", ",lake));
+        }
+    }
+}
