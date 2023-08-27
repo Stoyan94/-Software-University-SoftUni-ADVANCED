4131df2484273a7ad2c0d6ecd8d1275bf5908da7
Stoqn <podlobg@gmail.com>
2023-08-27 12:29:57 +0300

    ex

---
+++ Defining Classes-EX/Vehicle-Catalogue-OOP-2.0/Vehicle-Catalogue-OOP-2.0/CatalogVehicles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Vehicle_Catalogue_OOP_2._0
+{
+     class CatalogVehicles
+    {
+        public CatalogVehicles(string type,string model, string color, int horsePower)
+        {
+            this.Type = type;
+            this.Model = model;
+            this.Color = color;
+            this.HorsePower = horsePower;
+            this.Catalog = new List<CatalogVehicles>();
+        }
+
+        public List<CatalogVehicles> Catalog { get; set; }
+
+        public string Type { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public override string ToString()
+        {
+            return (@$"Type: {this.Type}
+Model: {this.Model} 
+Color: {this.Color} 
+HorsePower: {this.HorsePower}");
+        }
+
+    }
+}
---
+++ Defining Classes-EX/Vehicle-Catalogue-OOP-2.0/Vehicle-Catalogue-OOP-2.0/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Vehicle_Catalogue_OOP_2._0
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {      
+            List<CatalogVehicles> catalog = new List<CatalogVehicles>();
+
+            string vehicleInfo;
+
+            while ((vehicleInfo = Console.ReadLine()!) !="End")
+            {
+                string[] inputInfo = vehicleInfo.Split();
+
+                string typeVehicle = inputInfo[0];
+                string model = inputInfo[1];
+                string color  = inputInfo[2];
+                int horsePower = int.Parse(inputInfo[3]);
+
+                if (typeVehicle == "car") 
+                {
+                    CatalogVehicles car = new CatalogVehicles (typeVehicle,model,color,horsePower);
+                    catalog.Add(car);
+                }
+                else if (typeVehicle == "truck")
+                {
+                    CatalogVehicles truck = new CatalogVehicles(typeVehicle,model,color, horsePower);
+                    catalog.Add(truck);
+                }
+            }
+
+           
+            string vehiclelModel;
+            List<CatalogVehicles> getByModel =  new List<CatalogVehicles> ();
+
+            while ((vehiclelModel = Console.ReadLine()!)!= "Close the Catalogue")
+            {
+               CatalogVehicles findModel = catalog.Find(x=>x.Model == vehiclelModel)!;   
+               
+              getByModel.Add(findModel);
+            }            
+        
+         
+            foreach (var vehicles in getByModel)
+            {
+                Console.WriteLine($"{vehicles}");
+            }
+            
+
+            var carsCount = catalog.FindAll(x => x.Type == "car").Average(x=>x.HorsePower);
+            var trucksCount = catalog.FindAll(x => x.Type == "truck").Average(x=>x.HorsePower);
+            
+            
+            Console.WriteLine($"Cars have average horsepower of: {carsCount:f2}");
+            Console.WriteLine($"Trucks have average horsepower of: {trucksCount:f2}");
+        }
+    }
+}
