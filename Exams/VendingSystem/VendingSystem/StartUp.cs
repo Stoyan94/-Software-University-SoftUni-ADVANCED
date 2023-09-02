ee96fba61d04b6bf81cb55cb785afecea1435ba2
Stoqn <podlobg@gmail.com>
2023-09-02 17:30:50 +0300

    rew

--- Exams/VendingSystem/VendingSystem/StartUp.cs
+++ Exams/VendingSystem/VendingSystem/StartUp.cs
@@ -17,43 +17,29 @@ namespace VendingSystem
             Drink cappuccino = new Drink("Cappuccino", 2.8m, 180);
             Drink mocha = new Drink("Mocha", 2.1m, 150);
             Drink herbalTea = new Drink("Herbal Tea", 1.75m, 300);
-
-            //Get Count
+           
             Console.WriteLine(vendingMachine.GetCount());
-            //0
-
-            //Add Drinks
+         
             vendingMachine.AddDrink(tea);
             vendingMachine.AddDrink(coffee);
             vendingMachine.AddDrink(hotChocolate);
             vendingMachine.AddDrink(latte);
             vendingMachine.AddDrink(cappuccino);
             vendingMachine.AddDrink(mocha);
-
-            //Try to add drinks when the capacity is full
+           
             vendingMachine.AddDrink(herbalTea);
 
-            //Get Count
             Console.WriteLine(vendingMachine.GetCount());
-            //6
-
-            //Remove Drink
+           
             Console.WriteLine(vendingMachine.RemoveDrink("Herbal Tea"));
             Console.WriteLine(vendingMachine.RemoveDrink("Tea"));
 
-            //Get Longest Drink
             Console.WriteLine(vendingMachine.GetLongest());
-            
-            Console.WriteLine();
-            //Get Cheapest Drink
+         
             Console.WriteLine(vendingMachine.GetCheapest());
-           
-
-            //Buy a specific Drink
+        
             Console.WriteLine(vendingMachine.BuyDrink("Cappuccino"));
           
-
-            //Drinks Report
             Console.WriteLine(vendingMachine.Report());
         
 
