a344eb80c36231b93724afea1e7241746202d885
Stoqn <podlobg@gmail.com>
2023-09-02 17:29:33 +0300

    rew

--- Exams/VendingSystem/VendingSystem/Drink.cs
+++ Exams/VendingSystem/VendingSystem/Drink.cs
@@ -14,14 +14,9 @@ namespace VendingSystem
         }
 
         public string Name { get; set; }
-
         public decimal Price { get; set; }
-
         public int Volume { get; set; }
-
-        public override string ToString()
-        {
-            return $"Name: {this.Name}, Price: ${this.Price}, Volume: {this.Volume} ml";
-        }
+        public override string ToString()=> $"Name: {Name}, Price: ${Price}, Volume: {Volume} ml";
+        
     }
 }
--- Exams/VendingSystem/VendingSystem/VendingMachine.cs
+++ Exams/VendingSystem/VendingSystem/VendingMachine.cs
@@ -16,7 +16,7 @@ namespace VendingSystem
         public int ButtonCapacity { get; set; }
         public List<Drink> Drinks { get; set; }
 
-        public int GetCount() => Drinks.Count();
+        public int GetCount() => this.Drinks.Count();
 
         public void AddDrink(Drink drink)
         {
@@ -27,13 +27,26 @@ namespace VendingSystem
         }
 
         public bool RemoveDrink(string removeName)=>this.Drinks
-            .Remove(this.Drinks.First(x=>x.Name ==removeName));
-       
+            .Remove(this.Drinks.FirstOrDefault(x=>x.Name ==removeName));       
 
         public Drink GetLongest() => Drinks.OrderByDescending(x=>x.Volume).First();
-    
-        
 
+        public Drink GetCheapest() => Drinks.OrderBy(x => x.Price).First();
 
+        public string BuyDrink(string drinkName) => Drinks.FirstOrDefault(x=>x.Name == drinkName).ToString();
+      
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Drinks available:");
+
+            foreach(var drink in Drinks)
+            {
+                sb.AppendLine(drink.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
