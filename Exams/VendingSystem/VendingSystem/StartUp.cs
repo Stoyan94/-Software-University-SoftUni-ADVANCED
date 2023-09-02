f07a8d713ad51b8343abf8debdd68f3bab5498e8
Stoqn <podlobg@gmail.com>
2023-09-02 08:25:25 +0300

    ex

---
+++ Exams/VendingSystem/VendingSystem/StartUp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace VendingSystem
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //Initialize the repository (VendingMachine)
+            VendingMachine vendingMachine = new VendingMachine(6);
+
+            //Initialize entity (Drink)
+            Drink tea = new Drink("Tea", 1.5m, 300);
+            Drink coffee = new Drink("Coffee", 2.0m, 120);
+            Drink hotChocolate = new Drink("Hot Chocolate", 2.5m, 200);
+            Drink latte = new Drink("Latte", 3.5m, 220);
+            Drink cappuccino = new Drink("Cappuccino", 2.8m, 180);
+            Drink mocha = new Drink("Mocha", 2.1m, 150);
+            Drink herbalTea = new Drink("Herbal Tea", 1.75m, 300);
+
+            //Get Count
+            Console.WriteLine(vendingMachine.GetCount);
+            //0
+
+            //Add Drinks
+            vendingMachine.AddDrink(tea);
+            vendingMachine.AddDrink(coffee);
+            vendingMachine.AddDrink(hotChocolate);
+            vendingMachine.AddDrink(latte);
+            vendingMachine.AddDrink(cappuccino);
+            vendingMachine.AddDrink(mocha);
+
+            //Try to add drinks when the capacity is full
+            vendingMachine.AddDrink(herbalTea);
+
+            //Get Count
+            Console.WriteLine(vendingMachine.GetCount);
+            //6
+
+            //Remove Drink
+            Console.WriteLine(vendingMachine.RemoveDrink("Herbal Tea"));
+            Console.WriteLine(vendingMachine.RemoveDrink("Tea"));
+
+            //Get Longest Drink
+            Console.WriteLine(vendingMachine.GetLongest());
+            
+            Console.WriteLine();
+            //Get Cheapest Drink
+            Console.WriteLine(vendingMachine.GetCheapest());
+           
+
+            //Buy a specific Drink
+            Console.WriteLine(vendingMachine.BuyDrink("Cappuccino"));
+          
+
+            //Drinks Report
+            Console.WriteLine(vendingMachine.Report());
+        
+
+        }
+    }
+}
---
+++ Exams/VendingSystem/VendingSystem/VendingMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VendingSystem
+{
+    public class VendingMachine
+    {
+        public VendingMachine(int buttonCapacity)
+        {
+            this.ButtonCapacity = buttonCapacity;
+            Drinks = new List<Drink>();
+        }
+
+        public int ButtonCapacity { get; set; }
+        public List<Drink> Drinks { get; set; }
+
+        public int GetCount() => Drinks.Count();
+
+        public void AddDrink(Drink drink)
+        {
+            if(ButtonCapacity > Drinks.Count)
+            {           
+                this.Drinks.Add(drink);
+            }
+        }
+
+        public bool RemoveDrink(string removeName)=>this.Drinks
+            .Remove(this.Drinks.First(x=>x.Name ==removeName));
+       
+
+        public Drink GetLongest() => Drinks.OrderByDescending(x=>x.Volume).First();
+    
+        
+
+
+    }
+}
