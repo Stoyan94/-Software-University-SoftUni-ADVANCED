f00760a30389f073358b444121ecea93477e856c
Stoqn <podlobg@gmail.com>
2023-09-02 17:59:35 +0300

    ex

---
+++ Exams/Temple-of-Doom/Temple-of-Doom/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Temple_of_Doom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
