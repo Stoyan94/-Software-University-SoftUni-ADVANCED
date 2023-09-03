8d7de59a847fa6e609fd6a4a8fba2ae0d25faff9
Stoqn <podlobg@gmail.com>
2023-09-03 14:09:27 +0300

    rw

--- Exams/AutomotiveRepairShop/AutomotiveRepairShop/RepairShop.cs
+++ Exams/AutomotiveRepairShop/AutomotiveRepairShop/RepairShop.cs
@@ -10,5 +10,19 @@
 
         public int Capacity { get; set; }
         public List<Vehicle>? Vehicles { get; set; }
+
+        public int GetCount() => this.Vehicles!.Count();
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if(this.Vehicles!.Count > Capacity)
+            {
+                this.Vehicles.Add(vehicle);
+            }
+        }
+
+        public bool RemoveVehicle(string vin) => this.Vehicles!
+            .Remove(this.Vehicles.First(x => x.VIN == vin));
+
+        public Vehicle GetLowestMileage()=>this.Vehicles!.OrderBy(x=>x.Milage).First();
     }
 }
