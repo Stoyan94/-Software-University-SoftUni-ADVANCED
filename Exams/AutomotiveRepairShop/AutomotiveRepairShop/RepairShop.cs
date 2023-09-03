33808b0c9b3694a63e1cc1017f8341df0db01d1f
Stoqn <podlobg@gmail.com>
2023-09-03 13:41:18 +0300

    ex

---
+++ Exams/AutomotiveRepairShop/AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,14 @@
+﻿namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+        public RepairShop(int capacity)
+        {
+            this.Capacity = capacity;
+            Vehicles = new List<Vehicle>();
+        }
+
+        public int Capacity { get; set; }
+        public List<Vehicle>? Vehicles { get; set; }
+    }
+}
