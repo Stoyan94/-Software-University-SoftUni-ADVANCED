6c0119a49f2ba9a6f065f63d43ebcce407b4825a
Stoqn <podlobg@gmail.com>
2023-09-03 13:42:34 +0300

    ad

---
+++ Exams/AutomotiveRepairShop/AutomotiveRepairShop/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿namespace AutomotiveRepairShop
+{
+    public class Vehicle
+    {
+        public Vehicle(string? vIN, int milage, string? damage)
+        {
+            this.VIN = vIN;
+            this.Milage = milage;
+            this.Damage = damage;
+        }
+
+        public string? VIN { get; set; }
+        public int Milage { get; set; }
+        public string? Damage { get; set; }
+
+        public override string ToString()
+        {
+            return $"Damage: {this.Damage}, Vehicle: {this.VIN} ({this.Milage} km)";
+        }
+    }
+}
