57c00275fa4b872b3c26a2924ef373ae6af39fcc
Stoqn <podlobg@gmail.com>
2023-09-04 18:57:01 +0300

    ex

---
+++ CustomDataStructure/CustomDataStructure/CustomList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDataStructure
+{
+    public class CustomList
+    {
+        private int capacity;
+        private int[] data;
+
+        public CustomList()
+            : this(4)
+        {            
+        }
+
+        public CustomList(int capacity)
+        {
+            this.capacity = capacity;
+            this.data = new int[capacity];       
+        }
+        
+
+        public int Count { get; private set; }
+        public void Add(int num)
+        {
+            this.data[this.Count] = num;
+            this.Count++;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return this.data[index];
+            }
+            set
+            {
+                this.data[index] = value;
+            }
+        }
+
+        public void Clear()
+        {
+            this.Count = 0;
+            this.data = new int[capacity];
+        }
+    }
+}
---
+++ CustomDataStructure/CustomDataStructure/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CustomDataStructure
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {           
+            var csList = new CustomList();
+
+            csList.Add(10);
+            csList.Add(20);
+            csList.Add(30);
+
+            var cout = csList.Count;
+
+            Console.WriteLine(cout);
+
+            csList[0] = 100;
+            var number = csList[1];
+            Console.WriteLine(number);
+            Console.WriteLine(csList[2]);
+
+            csList.Clear();
+
+            Console.WriteLine(csList.Count);
+        }
+    }
+}
