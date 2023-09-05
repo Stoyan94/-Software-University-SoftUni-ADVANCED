0a74a1e693aa89e1e3c891bfe502d31d031c297b
Stoqn <podlobg@gmail.com>
2023-09-05 17:31:36 +0300

    ex

---
+++ CustomStack/CustomStack/CustomStack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class CustomStack
+    {
+        private int[] data;
+
+        public CustomStack()
+        {
+            this.data = new int[4];
+        }
+        public int Count { get;}
+        public void Push()
+        {
+
+        }
+        public int Pop()
+        {
+            return 0;
+        }
+
+        public int Peek()
+        {
+            return 0;
+        }
+
+        private void Resize()
+        {
+            var newCapacity = this.data.Length * 2;
+
+            var newData = new int[newCapacity];
+
+            for (int i = 0; i < this.data.Length; i++)
+            {
+                newData[i] = this.data[i];
+            }
+            this.data = newData;
+        }
+    }
+}
---
+++ CustomStack/CustomStack/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            var myStack = new CustomStack();
+
+            myStack.Push(10);
+
+            var result = myStack.Pop();
+
+            Console.WriteLine(result);
+
+            var peekResult = myStack.Peek();
+
+            Console.WriteLine(peekResult);
+
+            Console.WriteLine(myStack.Count);
+        }
+    }
+}
