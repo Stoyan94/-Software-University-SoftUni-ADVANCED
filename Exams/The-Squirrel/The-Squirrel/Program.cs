fdc1ad35537fdc7a9b96de6fbaea28baea8c8ddb
Stoqn <podlobg@gmail.com>
2023-09-08 20:12:39 +0300

    up

--- Exams/The-Squirrel/The-Squirrel/Program.cs
+++ Exams/The-Squirrel/The-Squirrel/Program.cs
@@ -5,17 +5,20 @@
         static void Main(string[] args)
         {
             int size = int.Parse(Console.ReadLine()!);
-
-            List<string> squCommands = new List<string>(Console.ReadLine()!.Split(","));
+                       
+            List<string> squCommands = Console.ReadLine()!.Split(", ").ToList();
 
             char[,] field = new char[size, size];
 
             int squRow = 0;
             int squCol = 0;
+            int allNutsCount = 0;
 
-            CreateFieldAndInfo(field, ref squRow, ref squCol);
+            CreateFieldAndInfo(field, ref squRow, ref squCol, ref allNutsCount);
 
-            int nutsCount = 0;
+           
+
+            int nutsCountCollected = 0;
 
 
             foreach (var command in squCommands)
@@ -27,39 +30,91 @@
 
                 switch (direction)
                 {
-                    case "up": nextRow = -1; break;
-                    case "down": nextRow = 1; break;
+                    case "up": nextRow = -1;   break;
+                    case "down": nextRow = 1;  break;
                     case "left": nextCol = -1; break;
-                    case "right": nextCol = 1; break;
+                    case "right": nextCol = 1; break;                   
                 }
 
-                if (isInRenage(field,squRow+nextRow,squCol+nextCol))
+                if (!isInRenage(field,squRow+nextRow,squCol+nextCol))
                 {
+                    OutFieldMeessage();
+                    break;
+                }
 
+                squRow += nextRow;
+                squCol += nextCol;
+
+                if (field[squRow, squCol] == 't')
+                {
+                    TrapMessage();
+                    break;
+                }
+                else
+                {
+                    if (field[squRow,squCol] == 'h')
+                    {
+                        nutsCountCollected++;
+                        allNutsCount--;
+
+                        field[squRow, squCol] = '*';
+
+                        if (nutsCountCollected == 3)
+                        {
+                            field[squRow, squCol] = 's';
+                            AllCollectedMessage();
+                            break;
+                        }
+                    }
                 }
             }
 
+            if (allNutsCount > 0)
+            {
+                Console.WriteLine("There are more hazelnuts to collect.");
+            }
+        }
+
+        public static void AllCollectedMessage()
+        {
+            Console.WriteLine("Good job! You have collected allhazelnuts!");
+        }
+
+        public static void TrapMessage()
+        {
+            Console.WriteLine("Unfortunately, the squirrel stepped on a trap...");
+        }
+
+        public static void OutFieldMeessage()
+        {
+            Console.WriteLine("The squirrel is out of the field.");
         }
 
         public static bool isInRenage(char[,] field, int row, int col)
         {
             return row >= 0 && row < field.GetLength(0) &&
-                   col >= 0 && col < field.GetLength(col);
+                   col >= 0 && col < field.GetLength(1);
         }
 
-        public static void CreateFieldAndInfo(char[,] field, ref int squRow, ref int squCol)
+        public static void CreateFieldAndInfo(char[,] field, ref int squRow,
+            ref int squCol,
+            ref int allNutsCount)
         {
             for (int row = 0; row < field.GetLength(0); row++)
             {
-                char[] fieldInput = Console.ReadLine()!.ToCharArray(); 
+                char[] fieldInput = Console.ReadLine()!.ToCharArray();
 
                 for (int col = 0; col < field.GetLength(1); col++)
                 {
-                    field[row,col] = fieldInput[col];
+                    field[row, col] = fieldInput[col];
 
-                    if (field[row,col] == 's')
+                    if (field[row, col] == 's')
                     {
                         squRow = row; squCol = col;
+                    }
+                    else if (field[row, col] == 'h')
+                    {
+                        allNutsCount++;
                     }
                 }
             }
