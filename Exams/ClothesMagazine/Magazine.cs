c7eb9f5938b36650d6ed2986540866c48e78b96e
Stoqn <podlobg@gmail.com>
2023-09-09 10:15:56 +0300

    ex

---
+++ Exams/ClothesMagazine/Magazine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ClothesMagazine
+{
+    public class Magazine
+    {
+        public Magazine(string type, int capacity)
+        {
+            this.Type = type;
+            this.Capacity = capacity;
+            this.Clothes = new List<Cloth>();
+        }
+
+        public string Type { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<Cloth> Clothes { get; set; }
+
+        public void AddCloth(Cloth cloth)
+        {
+            if (Clothes.Count < Capacity)
+            {
+                Clothes.Add(cloth);
+            }
+        }
+
+        public bool RemoveCloth(string color)=> this.Clothes.Remove
+            (this.Clothes.FirstOrDefault(x=>x.Color==color));
+    }
+}
