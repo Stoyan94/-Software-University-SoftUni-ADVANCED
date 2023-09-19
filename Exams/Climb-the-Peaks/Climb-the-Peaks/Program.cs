f09df5f4f4ce6d3a226adfd82d7f0cf03912a006
Stoqn <podlobg@gmail.com>
2023-09-19 12:07:17 +0300

    up

--- Exams/Climb-the-Peaks/Climb-the-Peaks/Program.cs
+++ Exams/Climb-the-Peaks/Climb-the-Peaks/Program.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 
-List<string> conquerPeaks = new List<string>();
+List<string> conquerdPeaks = new List<string>();
 
-Stack<int> foodPortions = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse)); 
+Stack<int> foodPortions = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse));
 
 Queue<int> stamina = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse));
 
-Dictionary<string,int> tableDif = new Dictionary<string, int>()
+Dictionary<string, int> tableDif = new Dictionary<string, int>()
 {
     ["Vihren"] = 80,
     ["Kutelo"] = 90,
@@ -19,8 +19,8 @@ Dictionary<string,int> tableDif = new Dictionary<string, int>()
 
 while (foodPortions.Any() || stamina.Any())
 {
-    int energy = foodPortions.Pop() + stamina.Dequeue();   
-    
+    int energy = foodPortions.Pop() + stamina.Dequeue();
+
 
     foreach (var peak in tableDif)
     {
@@ -36,19 +36,24 @@ while (foodPortions.Any() || stamina.Any())
             tableDif[peak.Key] = 0;
         }
 
-        if (tableDif[peak.Key] == 0) 
+        if (tableDif[peak.Key] == 0)
         {
-            conquerPeaks.Add(peak.Key);
+            conquerdPeaks.Add(peak.Key);
             tableDif.Remove(peak.Key);
         }
 
         break;
-    }  
+    }
 }
 
-Console.WriteLine(string.Join("\n",conquerPeaks));
-
+if (tableDif.Any())
+ {
+     Console.WriteLine("Alex failed! He has to organize his journey better next time -> @PIRINWINS");
+ }
+ else
+ {
+     Console.WriteLine($"Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK \nConquered peaks: \n{string.Join("\n",conquerdPeaks)}");
+ }
 
  
-
-
+        
