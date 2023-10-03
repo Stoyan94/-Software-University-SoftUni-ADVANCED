76ac5a2599fbcdfdc7028b5ec8a57b57c58d833e
Stoqn <podlobg@gmail.com>
2023-10-03 17:14:24 +0300

    ex

--- Exams/Basketball/Player.cs
+++ Exams/Basketball/Player.cs
@@ -4,15 +4,23 @@ namespace Basketball
 {
     public class Player
     {
+        public Player(string name, string position, double rating, int games)
+        {
+            Name = name;
+            Position = position;
+            Rating = rating;
+            Games = games;
+        }
+
         public string Name { get; set; }
         public string Position { get; set; }
         public double Rating { get; set; }
         public int Games { get; set; }
+        public bool Retired { get; set; }
+
 
 
        
-
-        //•	Retired: boolean – false by default
 
     }
 }
