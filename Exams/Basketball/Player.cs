995be2769918f4bad5815bfd7140ccb2ac0c5621
Stoqn <podlobg@gmail.com>
2023-10-03 17:10:18 +0300

    ex

---
+++ Exams/Basketball/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Basketball
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public double Rating { get; set; }
+        public int Games { get; set; }
+
+
+       
+
+        //•	Retired: boolean – false by default
+
+    }
+}
