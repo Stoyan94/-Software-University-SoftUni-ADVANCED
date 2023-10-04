933d895aa01be960eaa82df9982f688c3b44dbd5
Stoqn <podlobg@gmail.com>
2023-10-04 19:25:02 +0300

    ex

--- Exams/Basketball/Basketball/Team.cs
+++ Exams/Basketball/Basketball/Team.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Text;
 
 namespace Basketball
 {
@@ -60,13 +61,18 @@ namespace Basketball
         }
         
         
-        public int RemovePlayerByPosition(string playerPosition) => this.players.FindAll(x => x.Position == playerPosition).Count();
+        public int RemovePlayerByPosition(string playerPosition)
+        {
+            int count = this.players.RemoveAll(x => x.Position == playerPosition);
+
+            return count;
+        }
 
         public Player RetirePlayer(string playerName)
         {
             var player = this.players.FirstOrDefault(x => x.Name == playerName);
 
-            if(player != null)
+            if(player == null)
             {
                 return null;
             }
@@ -82,6 +88,15 @@ namespace Basketball
             return filteredPlayers;
         }
 
-
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Active players competing for Team {this.Name} from Group {this.Group}:");
+            foreach (var player in this.players.Where(x => x.Retired != true))
+            {
+                sb.AppendLine(player.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
     }
 }
