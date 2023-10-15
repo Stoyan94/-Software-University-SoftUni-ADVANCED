aa76c75c8497534116cd50615ff0815e038f7b60
Stoqn <podlobg@gmail.com>
2023-10-15 18:42:01 +0300

    ex

---
+++ Exams/Tiles-Master/Tiles-Master/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tiles_Master
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> whiteTiles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Queue<int> greyTiles = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Dictionary<string, int> table = new Dictionary<string,int>()
+            {
+                ["Sink"] = 40,                
+                ["Oven"] = 50,                
+                ["Countertop"] = 60,
+                ["Wall"] = 70,
+            };
+
+            Dictionary<string, int> typeAreaRenovation = new Dictionary<string, int>();
+
+            while (whiteTiles.Any() || greyTiles.Any())
+            {
+                int result = whiteTiles.Peek() + greyTiles.Peek();
+
+                if (table.ContainsValue(result))
+                {
+                    string nameArea = table.FirstOrDefault(x=>x.Value == result).Key;
+
+                    if (!typeAreaRenovation.ContainsKey(nameArea)) 
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
