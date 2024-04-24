cb1dfc1b3ff1638cc98dab6382c457e9912786f9
Stoyan <podlobg@gmail.com>
2024-04-24 11:19:27 +0300

    up

---
+++ Exams/Retake Exam - 10 April 2024/Escape-the-Maze/Escape-the-Maze/Program.cs
@@ -0,0 +1,41 @@
+﻿
+int size = int.Parse(Console.ReadLine());
+int health = 100;
+
+string[,] maze = new string[size, size];
+
+int playerRow = -1;
+int playerCol = -1;
+
+for (int i = 0; i < maze.GetLength(0); i++)
+{
+    string row = Console.ReadLine();
+    for (int j = 0; j < maze.GetLength(1); j++)
+    {
+        if (row[j].ToString() == "P")
+        {
+            playerRow = i;
+            playerCol = j;
+
+            maze[i, j] = "-";
+        }
+        else
+        {
+            maze[i, j] = row[j].ToString();
+        }
+    }
+}
+
+while (true)
+{
+    string direction = Console.ReadLine();
+
+    bool isOutOfBounds = DefineCommandIsOutOfBoundaries(maze, direction, playerRow, playerCol);
+
+    if (isOutOfBounds) continue;
+
+    PlayerStep(direction, ref playerRow, ref playerCol);
+
+    string position = maze[playerRow, playerCol];
+
+}
